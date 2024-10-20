830299f611eebbdf7c05b34d705c29c3459fdfa5
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-10-20 18:20:19 +0700

    add config

--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const controller = require('../controllers/indexController');
 
 // router.get('/createTables', (req, res) => {
-//   let models = require('../models');
-//   models.sequelize.sync().then(() => {
-//     res.send('tables created!');
-//   });
+//     let models = require('../models');
+//     models.sequelize.sync().then(() => {
+//         res.send('tables created!');
+//     });
 // });
 
 router.get('/', controller.showHomepage);
