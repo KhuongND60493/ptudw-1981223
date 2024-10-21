45d245571542fbf3a4946d135fb03a12aa0013a0
Khuong Nguyen <khuong.nguyen.duy@gosell.vn>
2024-10-21 08:35:30 +0700

    add validate for login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,26 @@
 'use strict';
 const models = require('../models');
+const passport = require('./passport');
 let controller = {};
 controller.showLogin = (req, res) => {
-    res.render('login');
+    res.render('login', {loginMessage: req.flash('loginMessage')});
 };
-controller.login = async (req, res) => {
+controller.login = async (req, res, next) => {
+    const keepSignedIn = req.body.keepSignedIn || false;
+    passport.authenticate('local-login', (error, user) => {
+        if (error) {
+            return next(error)
+        }
+        if (!user) {
+            return res.redirect('/users/login');
+        }
+        req.logIn(user, (e) => {
+            if (e) return next(e)
+            req.session.cookie.maxAge = keepSignedIn ? (24 * 60 * 60 * 1000) : null;
+            return res.redirect('/users/my-account')
+        })
+    })(req, res, next);
+
 };
 
 module.exports = controller;
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,16 @@
 let express = require('express');
 let router = express.Router();
 let controller = require('../controllers/authController');
+let {body, getErrorMessage} = require('../controllers/validator');
+
 router.get('/login', controller.showLogin);
-router.post('/login', controller.login);
+router.post('/login',
+    body('email').trim().notEmpty().withMessage('Email is required!!!').isEmail('Invalid email address!'),
+    body('password').trim().notEmpty().withMessage('Password is required!!!'), (req, res, next) => {
+        const errorMessage = getErrorMessage(req);
+        if (errorMessage) {
+            return res.render('login', {loginMessage: errorMessage});
+        }
+        next();
+    }, controller.login);
 module.exports = router;
